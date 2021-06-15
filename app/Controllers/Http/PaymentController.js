354729a18576836bd8f57f6fe2673e130d6b050e
ahhzaky <siahmadzaky@gmail.com>
2021-06-16 00:36:20 +0700

    :heavy_check_mark: add controller & model payment

---
+++ app/Controllers/Http/PaymentController.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const { validateAll } = use("Validator");
+const Campaign = use("App/Models/Campaign");
+const User = use("App/Models/User");
+const Payment = use("App/Models/Payment");
+const { v4: uuidv4 } = require("uuid");
+
+class PaymentController {}
+
+module.exports = PaymentController;
